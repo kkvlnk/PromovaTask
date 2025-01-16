851e356c9b8062a639eb6855737a4a5c713cae2a
kkovalenko <kk@empat.tech>
2025-01-16 18:46:27 +0200

    Add MovieEntity

---
+++ app/src/main/java/com/kovalenko/promovatask/data/local/entity/MovieEntity.kt
@@ -0,0 +1,37 @@
+package com.kovalenko.promovatask.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import kotlinx.serialization.json.Json
+
+@Entity(tableName = "movies")
+data class MovieEntity(
+    val adult: Boolean,
+    val backdropPath: String?,
+    val genreIds: List<Int>,
+    @PrimaryKey
+    val id: Int,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    val posterPath: String?,
+    val releaseDate: String,
+    val title: String,
+    val video: Boolean,
+    val voteAverage: Double,
+    val voteCount: Int
+)
+
+class GenresTypeConverter {
+    @TypeConverter
+    fun fromGenreIds(genreIds: List<Int>): String {
+        return Json.encodeToString<List<Int>>(genreIds)
+    }
+
+    @TypeConverter
+    fun toGenreIds(genreIdsString: String): List<Int> {
+        return Json.decodeFromString<List<Int>>(genreIdsString)
+    }
+}
