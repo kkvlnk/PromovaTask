eef69e7a08bc08befda2d9e1cac85d1ff2e8dd06
kkovalenko <kk@empat.tech>
2025-01-16 19:54:00 +0200

    Add GenreDao

---
+++ app/src/main/java/com/kovalenko/promovatask/data/local/dao/GenreDao.kt
@@ -0,0 +1,19 @@
+package com.kovalenko.promovatask.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.kovalenko.promovatask.data.local.entity.GenreEntity
+
+@Dao
+interface GenreDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertGenres(genres: List<GenreEntity>)
+
+    @Query("SELECT * FROM genres")
+    suspend fun getAllGenres(): List<GenreEntity>
+
+    @Query("DELETE FROM genres")
+    suspend fun clearAll()
+}
