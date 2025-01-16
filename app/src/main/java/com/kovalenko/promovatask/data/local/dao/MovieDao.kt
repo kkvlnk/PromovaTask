9a21af55a1f4bffcfa65a795247130f3f9c1c079
kkovalenko <kk@empat.tech>
2025-01-16 19:54:24 +0200

    Add MovieDao

---
+++ app/src/main/java/com/kovalenko/promovatask/data/local/dao/MovieDao.kt
@@ -0,0 +1,27 @@
+package com.kovalenko.promovatask.data.local.dao
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.kovalenko.promovatask.data.local.entity.MovieEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MovieDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovies(movies: List<MovieEntity>)
+
+    @Query("SELECT * FROM movies")
+    suspend fun getAllMovies(): List<MovieEntity>
+
+    @Query("SELECT * FROM movies")
+    suspend fun getMoviesPaging(): PagingSource<Int, MovieEntity>
+
+    @Query("SELECT * FROM movies WHERE isLiked = 1")
+    fun getLikedMovies(): Flow<List<MovieEntity>>
+
+    @Query("DELETE FROM movies")
+    suspend fun clearAll()
+}
