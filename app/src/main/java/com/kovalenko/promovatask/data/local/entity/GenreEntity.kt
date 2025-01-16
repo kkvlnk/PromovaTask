55835ce5d3c199e69f414ade5ad58977e33d1ba6
kkovalenko <kk@empat.tech>
2025-01-16 21:02:15 +0200

    Annotate id with PrimaryKey

--- app/src/main/java/com/kovalenko/promovatask/data/local/entity/GenreEntity.kt
+++ app/src/main/java/com/kovalenko/promovatask/data/local/entity/GenreEntity.kt
@@ -1,9 +1,11 @@
 package com.kovalenko.promovatask.data.local.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "genres")
 data class GenreEntity(
+    @PrimaryKey
     val id: Int,
     val name: String
 )
