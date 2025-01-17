965c273d228d10713cfb5ebf14032a6c3a1a42f6
kkovalenko <kk@empat.tech>
2025-01-17 13:56:21 +0200

    Add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.androidx.navigation.compose)
 
 //    Serialization
     implementation(libs.kotlin.serialization.json)
