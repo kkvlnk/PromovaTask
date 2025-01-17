05f6154c984b09107a3e107305653d77aa1f84d8
kkovalenko <kk@empat.tech>
2025-01-17 14:26:04 +0200

    Add PromovaTaskApp

--- app/src/main/java/com/kovalenko/promovatask/presentation/MainActivity.kt
+++ app/src/main/java/com/kovalenko/promovatask/presentation/MainActivity.kt
@@ -4,44 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kovalenko.promovatask.presentation.ui.theme.PromovaTaskTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PromovaTaskTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            PromovaTaskApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PromovaTaskTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/kovalenko/promovatask/presentation/PromovaTaskApp.kt
@@ -0,0 +1,51 @@
+package com.kovalenko.promovatask.presentation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.kovalenko.promovatask.R
+import com.kovalenko.promovatask.presentation.navigation.Favorites
+import com.kovalenko.promovatask.presentation.navigation.Movies
+import com.kovalenko.promovatask.presentation.navigation.PromovaTaskNavHost
+import com.kovalenko.promovatask.presentation.navigation.TopLevelRoute
+import com.kovalenko.promovatask.presentation.ui.components.BottomNavBar
+import com.kovalenko.promovatask.presentation.ui.theme.PromovaTaskTheme
+
+@Composable
+fun PromovaTaskApp() {
+    PromovaTaskTheme {
+        val navController = rememberNavController()
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        Scaffold(
+            bottomBar = {
+                val routes = remember {
+                    listOf(
+                        TopLevelRoute(R.string.movies, Movies, Icons.AutoMirrored.Filled.List),
+                        TopLevelRoute(R.string.favorites, Favorites, Icons.Filled.Favorite),
+                    )
+                }
+                BottomNavBar(routes = routes, currentDestination = currentDestination) {
+                    navController.navigate(it.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            }
+        ) { innerPadding ->
+            PromovaTaskNavHost(navController, Modifier.padding(innerPadding))
+        }
+    }
+}
