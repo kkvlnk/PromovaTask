2cd39dbb313f5696af22e83922bf137fb32b0de9
kkovalenko <kk@empat.tech>
2025-01-17 13:32:50 +0200

    Disable dynamic color

--- app/src/main/java/com/kovalenko/promovatask/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/kovalenko/promovatask/presentation/ui/theme/Theme.kt
@@ -452,7 +452,7 @@ private val highContrastDarkColorScheme = darkColorScheme(
 fun PromovaTaskTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable() () -> Unit
 ) {
     val colorScheme = when {
