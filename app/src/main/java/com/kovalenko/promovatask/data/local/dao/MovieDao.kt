852261d89942739be062f2b346e011da7e2fbe20
kkovalenko <kk@empat.tech>
2025-01-19 01:27:57 +0200

    Replace Insert with Upsert for movies insertion

--- app/src/main/java/com/kovalenko/promovatask/data/local/dao/MovieDao.kt
+++ app/src/main/java/com/kovalenko/promovatask/data/local/dao/MovieDao.kt
@@ -2,17 +2,16 @@ package com.kovalenko.promovatask.data.local.dao
 
 import androidx.paging.PagingSource
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Upsert
 import com.kovalenko.promovatask.data.local.entity.MovieEntity
 import com.kovalenko.promovatask.data.local.entity.MovieWithGenres
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MovieDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Upsert
     suspend fun insertMovies(movies: List<MovieEntity>)
 
     @Query("SELECT * FROM movies")
