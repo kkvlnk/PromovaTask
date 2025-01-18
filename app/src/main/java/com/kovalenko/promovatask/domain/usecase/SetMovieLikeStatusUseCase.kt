aee590be5f5f302c32ae8a9583420d4621fe8b2f
kkovalenko <kk@empat.tech>
2025-01-19 01:28:08 +0200

    Add SetMovieLikeStatusUseCase

---
+++ app/src/main/java/com/kovalenko/promovatask/domain/usecase/SetMovieLikeStatusUseCase.kt
@@ -0,0 +1,11 @@
+package com.kovalenko.promovatask.domain.usecase
+
+import com.kovalenko.promovatask.domain.repository.MovieRepository
+import org.koin.core.annotation.Factory
+
+@Factory
+class SetMovieLikeStatusUseCase(private val repository: MovieRepository) {
+    suspend operator fun invoke(movieId: Int, liked: Boolean) {
+        repository.updateLikedStatus(movieId = movieId, liked = liked)
+    }
+}
