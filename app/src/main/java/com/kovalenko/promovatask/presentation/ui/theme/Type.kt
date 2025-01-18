1cb53b81f5040035d17d16175a38eb7fac36b9be
kkovalenko <kk@empat.tech>
2025-01-19 00:10:25 +0200

    Update display font family

--- app/src/main/java/com/kovalenko/promovatask/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/kovalenko/promovatask/presentation/ui/theme/Type.kt
@@ -22,7 +22,7 @@ val bodyFontFamily = FontFamily(
 
 val displayFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Inter"),
+        googleFont = GoogleFont("Poppins"),
         fontProvider = provider,
     )
 )
