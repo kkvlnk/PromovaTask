1ef98e267a63e0b0ce6e1ed47485118a68b2b021
kkovalenko <kk@empat.tech>
2025-01-20 14:27:40 +0200

    Add observeGenres method to GenreDao

--- app/src/main/java/com/kovalenko/promovatask/data/local/dao/GenreDao.kt
+++ app/src/main/java/com/kovalenko/promovatask/data/local/dao/GenreDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.kovalenko.promovatask.data.local.entity.GenreEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GenreDao {
@@ -13,6 +14,9 @@ interface GenreDao {
 
     @Query("SELECT * FROM genres")
     suspend fun getGenres(): List<GenreEntity>
+
+    @Query("SELECT * FROM genres")
+    fun observeGenres(): Flow<List<GenreEntity>>
 
     @Query("DELETE FROM genres")
     suspend fun clearAll()
