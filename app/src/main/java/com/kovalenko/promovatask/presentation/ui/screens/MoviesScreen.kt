64171cb131d276e5f26a8ca647346e8d89b9663a
kkovalenko <kk@empat.tech>
2025-01-20 15:41:05 +0200

    Add ErrorContent component, show loading states

--- app/src/main/java/com/kovalenko/promovatask/presentation/ui/screens/MoviesScreen.kt
+++ app/src/main/java/com/kovalenko/promovatask/presentation/ui/screens/MoviesScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,13 +22,17 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemContentType
 import androidx.paging.compose.itemKey
 import com.kovalenko.promovatask.R
 import com.kovalenko.promovatask.domain.model.Movie
+import com.kovalenko.promovatask.presentation.ui.components.MovieErrorItem
 import com.kovalenko.promovatask.presentation.ui.components.MovieListItem
+import com.kovalenko.promovatask.presentation.ui.components.MovieLoadingItem
+import com.kovalenko.promovatask.presentation.viewmodel.ErrorMessage
 import com.kovalenko.promovatask.presentation.viewmodel.MoviesAction
 import com.kovalenko.promovatask.presentation.viewmodel.MoviesScreenUiState
 import com.kovalenko.promovatask.presentation.viewmodel.MoviesViewModel
@@ -59,11 +64,18 @@ fun MoviesScreen(
         Box(Modifier.padding(innerPadding)) {
             when (uiState) {
                 MoviesScreenUiState.Loading -> LoadingContent()
-                MoviesScreenUiState.EmptyGenres -> EmptyGenresContent(
-                    refresh = { performAction(MoviesAction.RefreshGenres) }
+                MoviesScreenUiState.EmptyGenres -> ErrorContent(
+                    text = stringResource(R.string.empty_genres_message),
+                    retry = { performAction(MoviesAction.RefreshGenres) }
                 )
 
-                is MoviesScreenUiState.Error -> TODO()
+                is MoviesScreenUiState.Error -> ErrorContent(
+                    text = when (val message = uiState.errorMessage) {
+                        is ErrorMessage.ResourceMessage -> stringResource(message.message)
+                        is ErrorMessage.StringMessage -> message.message
+                        null -> stringResource(R.string.error_unknown)
+                    }
+                )
 
                 is MoviesScreenUiState.Movies -> MoviesContent(
                     movies = movies,
@@ -82,32 +94,29 @@ fun MoviesContent(
     setLikeStatus: (Int, Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        modifier = modifier,
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 24.dp)
-    ) {
-        items(
-            count = movies.itemCount,
-            key = movies.itemKey(key = { it.id }),
-            contentType = movies.itemContentType()
-        ) { index ->
-            val movie = movies[index]
-            if (movie != null) {
-                MovieListItem(movie, setLikeStatus = setLikeStatus)
+    if (movies.loadState.refresh is LoadState.Loading && movies.itemCount == 0) {
+        LoadingContent()
+    } else {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = modifier,
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 24.dp)
+        ) {
+            items(
+                count = movies.itemCount,
+                key = movies.itemKey(key = { it.id }),
+                contentType = movies.itemContentType()
+            ) { index ->
+                val movie = movies[index]
+                if (movie != null) {
+                    MovieListItem(movie, setLikeStatus = setLikeStatus)
+                }
             }
-        }
-    }
-}
 
-@Composable
-fun EmptyGenresContent(modifier: Modifier = Modifier, refresh: () -> Unit) {
-    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = stringResource(R.string.empty_genres_message), textAlign = TextAlign.Center)
-            Spacer(Modifier.height(12.dp))
-            Button(onClick = refresh) {
-                Text(text = stringResource(R.string.refresh))
+            when (movies.loadState.append) {
+                is LoadState.Error -> item { MovieErrorItem(retry = movies::retry) }
+                LoadState.Loading -> item { MovieLoadingItem() }
+                else -> Unit
             }
         }
     }
@@ -117,5 +126,28 @@ fun EmptyGenresContent(modifier: Modifier = Modifier, refresh: () -> Unit) {
 fun LoadingContent(modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun ErrorContent(
+    text: String,
+    modifier: Modifier = Modifier,
+    retry: (() -> Unit)? = null
+) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = text,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleSmall
+            )
+            if (retry != null) {
+                Spacer(Modifier.height(12.dp))
+                Button(onClick = retry) {
+                    Text(text = stringResource(R.string.retry))
+                }
+            }
+        }
     }
 }
